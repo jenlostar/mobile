1af18cb9b05f9cbfceb1ed5f77a2846533d0b4a4
Jenny Lopez <jenlostar@gmail.com>
2014-11-03 22:41:43 -0500

    Removiendo selección de servicios desde reservas

--- app/controllers/lugar_horario.js
+++ app/controllers/lugar_horario.js
@@ -1,8 +1,6 @@
 var parametros = arguments[0] || {};
 
 var moment = require('alloy/moment');
-moment().lang('es');
-
 var fechaActual = new Date();
 var anioActual = fechaActual.getFullYear();
 var mesActual = fechaActual.getMonth();
@@ -12,12 +10,6 @@ var fechaMaxima = new Date(anioActual, mesActual, diaActual + 15, 7, 0, 0);
 
 var fechaSeleccionada = fechaMinima;
 
-var servicios = _.groupBy(Alloy.Globals.lugar.services, 'kind');
-var tiposServicio = _.map(_.keys(servicios), function(servicio) {
-    return servicio;
-});
-
-var dialogoAbierto = false;
 var horaSeleccionada = null;
 
 function cargarLista() {
@@ -71,71 +63,6 @@ function enviarReserva(servicios) {
     xhr.send(JSON.stringify(post));
 }
 
-function dialogoTiposServicio(e) {
-    if (dialogoAbierto === true || (e.bindId && e.bindId == 'hora')) { return; }
-    dialogoAbierto = true;
-
-    horaSeleccionada = e.section.getItemAt(e.itemIndex).fecha;
-
-    var ventanaDialogo = Ti.UI.createOptionDialog({
-        persistent: true,
-        cancel: -1,
-        title: 'Tipo de servicio',
-        options: tiposServicio,
-        buttonNames: ['Cancelar']
-    });
-
-    ventanaDialogo.addEventListener('click', dialogoServicios);
-    ventanaDialogo.show();
-}
-
-function dialogoServicios(e) {
-    if (e.cancel === true) {
-        dialogoAbierto = false;
-        return;
-    }
-    var tipoServicio = tiposServicio[e.index];
-
-    var serviciosPorTipo = _.where(Alloy.Globals.lugar.services, {
-        kind: tipoServicio
-    });
-
-    var Dialogs = require('yy.tidialogs');
-
-    var listadoServicios = Dialogs.createMultiPicker({
-        title: 'Servicios para ' + tipoServicio,
-        options: _.map(serviciosPorTipo, function(servicio) {
-            return servicio.name;
-        }),
-        okButtonTitle: 'Continuar',
-        cancelButtonTitle : 'Cancelar'
-    });
-
-    listadoServicios.addEventListener('click',function(e) {
-        var indexes = e.indexes;
-        var serviciosSeleccionados = [];
-
-        _.each(serviciosPorTipo, function(servicio, index) {
-            if (_.indexOf(indexes, index) >= 0) {
-                serviciosSeleccionados.push(servicio);
-            }
-        });
-
-        dialogoAbierto = false;
-
-        enviarReserva(_.map(serviciosSeleccionados, function(servicio) {
-            return servicio.id;
-        }));
-    });
-
-    listadoServicios.addEventListener('cancel', function() {
-        Ti.API.info('dialog was cancelled');
-        dialogoAbierto = false;
-    });
-
-    listadoServicios.show();
-}
-
 function actualizarInfoNavegacion() {
     var abx = require('com.alcoapps.actionbarextras'),
         fechaActual = new moment(fechaSeleccionada);
@@ -169,4 +96,6 @@ $.anterior.addEventListener('click', function() {
     cargarLista();
 });
 
-$.controlLista.addEventListener('itemclick', dialogoTiposServicio);
+$.controlLista.addEventListener('itemclick', function() {
+
+});
